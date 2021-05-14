592949f2866f3554e0e2d0122de89d5c2b6504d6
Ali Osman <amrali975@gmail.com>
2021-05-14 17:50:28 +0200

    added admins table generated scoped views and was able to call the name column from products controller index into views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,7 @@ class ProductsController < ApplicationController
   # GET /products or /products.json
   def index
     @products = Product.all
+    @admin = current_admin
   end
 
   # GET /products/1 or /products/1.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :product_attachments
   resources :products
   resources :brands
