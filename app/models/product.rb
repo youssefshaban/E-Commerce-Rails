0bfa864c83aaea1f67b950e60eac8c81b6481ca6
Ali Osman <amrali975@gmail.com>
2021-05-15 20:48:05 +0200

    Products issue resolved made foreign keys optional

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,13 +2,13 @@ class Product < ApplicationRecord
     has_many :ProductAttachment
     accepts_nested_attributes_for :ProductAttachment
     
-    belongs_to :Store 
+    belongs_to :Store , optional: true
 
     has_many :CartCarry
     has_many :Cart, through: :CartCarry
 
-    belongs_to :Brand
-    belongs_to :Category
+    belongs_to :Brand , optional: true
+    belongs_to :Category , optional: true
 
 
     has_many :OrderItems
