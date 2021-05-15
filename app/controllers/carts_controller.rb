ca4d2e8e937f279679bc6bbb1354947eaadf5f4a
Ali Osman <amrali975@gmail.com>
2021-05-15 20:43:03 +0200

    Getting pulls

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -106,9 +106,16 @@ class CartsController < ApplicationController
     @cart = Cart.find_by(buyer_id:current_buyer.id)
     @items = CartCarry.where(cart_id:@cart.id)
     if @items != nil
-      @order = Order.create(state: "Pending", buyer_id:current_buyer.id)
       @items.each do |item|
         @product_for_this_item = Product.find_by(id: item.product_id)
+        if @product_for_this_item.quantity >= item.quantity
+        else
+          redirect_to root_path
+          return
+        end
+      end
+      @order = Order.create(state: "Pending", buyer_id:current_buyer.id)
+      @items.each do |item|
         @store = Store.find_by(id: @product_for_this_item.store_id)
         @seller = Seller.find_by(store_id: @store.id)
         @order_items = OrderItem.create(state: "Pending", priceAtBuyTime:@product_for_this_item.currentPrice ,
